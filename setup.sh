1d9ffb7a493cb51e06f3ee3e2164d478e78b8e56
Nebula-gmk <94028618+Nebula-gmk@users.noreply.github.com>
2021-12-29 12:01:45 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,20 +2,37 @@
 echo ''
 clear
 echo ''
-echo '═══════════════════════════════════════════════════════════ '
-echo -e "\e[92m             ██╗  ██╗██╗  ██╗███████╗███████╗           "
-echo -e "\e[92m             ██║  ██║██║  ██║╚══███╔╝╚══███╔╝           "
-echo -e "\e[92m             ███████║███████║  ███╔╝   ███╔╝            "
-echo -e "\e[92m             ██╔══██║╚════██║ ███╔╝   ███╔╝             "
-echo -e "\e[92m             ██║  ██║     ██║███████╗███████╗           "
-echo -e "\e[92m             ╚═╝  ╚═╝     ╚═╝╚══════╝╚══════╝           "
-echo '------------------------------------------------------------ '
+echo '  ____ _____ _____ ____ _   _      ____ ___ ____ ___ _  __  '
+echo ' / ___|_   _| ____/ ___| | | |    |  _ \_ _|  _ \_ _| |/ /  '
+echo '| |     | | |  _|| |   | |_| |    | | | | || | | | || ' /  ''
+echo '| |___  | | | |__| |___|  _  |    | |_| | || |_| | || . \ '''
+echo ' \____| |_| |_____\____|_| |_|    |____/___|____/___|_|\_\  '
+echo '                 Free Script untuk semua!                   '
 echo ''
-echo '                   Telegram: @harithwyd                      '
-echo '                          AUTOSCRIPT                         '
+echo '                    Haram Untuk Dijual                      '
+echo '                 Siapa beli, dia kena scam                  '
 echo ''
-echo '║                       5 sec remaining                     ║ '
-echo '╚═══════════════════════════════════════════════════════════╝ '
+echo ' .......................................................... '
+echo '                 Autoscript By CTech Didik'
+echo '                 Original Script by SamVPN                  '
+echo '                        Menu by RARE                        '
+echo ''
+echo '                       Build in 2021                        '
+echo ''
+echo '                    Youtube: CTechDidik                     '
+echo '                Like dan Subscribe sekarang                 '
+echo ''
+echo ''
+echo '                         Telegram                           '
+echo '              Telegram Channel t.me/ctechdidik              '
+echo '             Telegram Owner CTech Didik? rahsia             '
+echo '                  Telegram SamVPN @sam_sfx                  '
+echo '                 Telegram SamVPN @kedairare                 '
+echo ''
+echo ''
+echo ''
+echo '                       Tunggu 5 Saat!                       '
+echo ' .......................................................... '
 sleep 5
 clear
 if [ "${EUID}" -ne 0 ]; then
@@ -29,66 +46,89 @@ fi
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-
-if [ true ]; then
+MYIP=$(wget -qO- ipinfo.io/ip);
+IZIN=$( curl https://raw.githubusercontent.com/muhamadparizan/kebenaran/main/ipdibenarkan | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
 clear
-echo -e  "═══════════════════════════════════════════════════════════"
-echo ""
-echo -e  "${green}Tahniah! Anda Dibenarkan menggunakan script ini...${NC}"
-echo -e  "${NC}       Premium Script By @harithwyd    "
-echo ""
-echo -e  "═══════════════════════════════════════════════════════════"
-sleep 4
+echo -e "${green}Tahniah! Anda Dibenarkan menggunakan Script Ctech Didik...${NC}"
+sleep 5
 else
 clear
 echo ""
-echo -e  "  ═══════════════════════════════════════════════════════════  "
-echo -e  "           You Are Not Allowed To Use This Script!           ║ "
-echo -e  "  ---------------------------------------------------------- ║ "
-echo -e  "                       t.me/harithwyd                        ║ "
-echo -e  "  ═══════════════════════════════════════════════════════════  "
-rm -rf setup.sh
+rm -f setup.sh
+echo '                            ...                           '
+echo '        Anda Tidak Dibenarkan Menggunakan Script ini!     '
+echo '                            ...                           '
+echo '    Tonton video Youtube saya dan dapatkan kebenaran!     '
+echo '   copy link utube ini:  https://youtu.be/gWk_k3SoaoI     '
+echo '                  Telegram t.me/ctechdidik                '
+sleep 20
 exit 0
 fi
+echo '============================================='
+echo '          Sila Tunggu sebentar '
+echo 'Process Update & Upgrade Sedang Dijalankan '
+echo '============================================='
+sleep 2
+apt update && apt upgrade -y
 clear
-if [ -f "/etc/v2ray/domain" ]; then
-echo "Script Already Installed"
-exit 0
-fi
+echo '============================================='
+echo '      Process Update&Upgrade Selesai '
+echo '============================================='
+sleep 2
+clear
+echo '============================================='
+echo '        Process Seterusnya Sila Tunggu '
+echo '============================================='
+sleep 2
+mkdir /etc/v2ray
 mkdir /var/lib/premium-script;
-echo "Enter the VPS Subdomain Hostname, if not available, please click Enter"
+mkdir /var/lib/crot-script;
+clear
+echo '============================================='
+echo "Sila Masukkan DOMAIN, Jika TIADA KLIK Enter"
+echo '============================================='
 read -p "Hostname / Domain: " host
 echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
-echo -e "$green   =============================================$NC"
-echo -e "$green               Sila Tunggu sebentar             $NC"
-echo -e "$green    Process Update & Upgrade Sedang Dijalankan  $NC"
-echo -e "$green   =============================================$NC"
-sleep 2
-apt-get update && apt-get upgrade -y && update-grub -y
+echo "IP=$host" >> /var/lib/crot-script/ipvps.conf
+echo "$host" >> /etc/v2ray/domain
 clear
-echo -e "$green   =============================================$NC"
-echo -e "$green          Process Update&Upgrade Selesai        $NC"
-echo -e "$green   =============================================$NC"
-sleep 2
-clear
-echo -e "$green   =============================================$NC"
-echo -e "$green         Installing AutoScript Harithwyd        $NC"
-echo -e "$green   =============================================$NC"
-sleep 2
-wget https://raw.githubusercontent.com/XC0D3-X/ScriptBuruk/main/cf.sh && chmod +x cf.sh && ./cf.sh
-#install ssh ovpn
-wget https://raw.githubusercontent.com/XC0D3-X/ScriptBuruk/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
-wget https://raw.githubusercontent.com/XC0D3-X/ScriptBuruk/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
-#install ssr
-wget https://raw.githubusercontent.com/XC0D3-X/ScriptBuruk/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
-wget https://raw.githubusercontent.com/XC0D3-X/ScriptBuruk/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
-#install wg
-wget https://raw.githubusercontent.com/XC0D3-X/ScriptBuruk/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
-#install v2ray
-wget http://raw.githubusercontent.com/XC0D3-X/ScriptBuruk/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
-#install L2TP
-wget https://raw.githubusercontent.com/XC0D3-X/ScriptBuruk/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
-wget https://raw.githubusercontent.com/XC0D3-X/ScriptBuruk/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+echo '============================================='
+echo '        Installing SSH & OPENVPN '
+echo '============================================='
+wget https://raw.githubusercontent.com/muhamadparizan/ahahk/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+echo '============================================='
+echo '        Installing SSTP '
+echo '============================================='
+wget https://raw.githubusercontent.com/muhamadparizan/ahahk/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
+echo '============================================='
+echo '        Installing ssr '
+echo '============================================='
+wget https://raw.githubusercontent.com/muhamadparizan/ahahk/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
+echo '============================================='
+echo '        Installing shadowsocksobfs '
+echo '============================================='
+wget https://raw.githubusercontent.com/muhamadparizan/ahahk/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
+echo '============================================='
+echo '        Installing WIREGUARD '
+echo '============================================='
+wget https://raw.githubusercontent.com/muhamadparizan/ahahk/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
+echo '============================================='
+echo '        Installing V2RAY '
+echo '============================================='
+wget https://raw.githubusercontent.com/muhamadparizan/ahahk/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
+echo '============================================='
+echo '        Installing ipsec '
+echo '============================================='
+wget https://raw.githubusercontent.com/muhamadparizan/ahahk/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
+echo '        Installing XRAY '
+echo '============================================='
+wget https://raw.githubusercontent.com/muhamadparizan/ahahk/main/install-xray.sh && chmod +x install-xray.sh && screen -S v2ray ./install-xray.sh
+echo '============================================='
+echo '        Installing Websocket '
+echo '============================================='
+wget https://raw.githubusercontent.com/muhamadparizan/ahahk/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+
 
 rm -f /root/ssh-vpn.sh
 rm -f /root/sstp.sh
@@ -96,30 +136,29 @@ rm -f /root/wg.sh
 rm -f /root/ss.sh
 rm -f /root/ssr.sh
 rm -f /root/ins-vt.sh
+rm -f /root/install-xray.sh
 rm -f /root/ipsec.sh
 rm -f /root/set-br.sh
-cat <<EOF> /etc/systemd/system/autosett.service
-[Unit]
-Description=autosetting
-Documentation=https://vpnstores.net
+echo "1.1" > /home/ver
 
-[Service]
-Type=oneshot
-ExecStart=/bin/bash /etc/set.sh
-RemainAfterExit=yes
-
-[Install]
-WantedBy=multi-user.target
-EOF
-systemctl daemon-reload
-systemctl enable autosett
-wget -O /etc/set.sh "https://raw.githubusercontent.com/XC0D3-X/ScriptBuruk/main/set.sh"
-chmod +x /etc/set.sh
-history -c
-echo "1.2" > /home/ver
+echo "echo '                Dibina oleh CTechDidik             '" >> .profile
+echo "echo ''" >> .profile
+echo "echo '                 Youtube: CTech Didik              '" >> .profile
+echo "echo '     Telegram Channel: https://t.me/ctechdidik     '" >> .profile
+echo "echo ''" >> .profile
+echo "echo '              Raw Script oleh @sam_sfx             '" >> .profile
+echo "echo '             Menu Style oleh @kedairare            '" >> .profile
+echo "echo ''" >> .profile
+echo "echo '       Sila Type: 'menu', untuk ke menu utama      '" >> .profile
+echo "echo ''" >> .profile
+echo "echo '    Sila Type: 'info', untuk melihat info skrip    '" >> .profile
+echo "echo ''" >> .profile
 clear
 echo " "
-echo "Installation has been completed!!"
+echo '============================================='
+echo "       Installation has been completed!!"
+echo '============================================='
+sleep 3
 echo " "
 echo "=================================-Autoscript Premium-===========================" | tee -a log-install.txt
 echo "" | tee -a log-install.txt
@@ -144,16 +183,18 @@ echo "   - V2RAY Vmess TLS         : 8443"  | tee -a log-install.txt
 echo "   - V2RAY Vmess None TLS    : 80"  | tee -a log-install.txt
 echo "   - V2RAY Vless TLS         : 2083"  | tee -a log-install.txt
 echo "   - V2RAY Vless None TLS    : 8880"  | tee -a log-install.txt
+echo "   - XRAY DIRECT             : 6769"  | tee -a log-install.txt
+echo "   - XRAY SPLICE             : 6769"  | tee -a log-install.txt
 echo "   - Trojan                  : 2087"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
-echo "   - Timezone                : Asia/Singapore (GMT +8)"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Malaysia (UTC +8)"  | tee -a log-install.txt
 echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
 echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
 echo "   - IPtables                : [ON]"  | tee -a log-install.txt
 echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
 echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
-echo "   - Autoreboot On 05.00 GMT +7" | tee -a log-install.txt
+echo "   - Autoreboot On 05.00 UTC +8" | tee -a log-install.txt
 echo "   - Autobackup Data" | tee -a log-install.txt
 echo "   - Restore Data" | tee -a log-install.txt
 echo "   - Auto Delete Expired Account" | tee -a log-install.txt
@@ -161,14 +202,23 @@ echo "   - Full Orders For Various Services" | tee -a log-install.txt
 echo "   - White Label" | tee -a log-install.txt
 echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
-echo "   - Dev/Main                : Haz / harithwyd"  | tee -a log-install.txt
-echo "   - Telegram                : t.me/Harithwyd"  | tee -a log-install.txt
-echo "   - Instagram               : ytjt "  | tee -a log-install.txt
-echo "   - Whatsapp                : ytjt "  | tee -a log-install.txt
-echo "   - Facebook                : ytjt " | tee -a log-install.txt
-echo "---------------------Script Created By XC0D3-X--------------------" | tee -a log-install.txt
 echo ""
-echo " Reboot 15 Sec"
+echo "   - 1st Dev/Main            : CTechDidik"  | tee -a log-install.txt
+echo "   - 1st Dev/Youtube         : CTech Didik -Subscribe Now!"  | tee -a log-install.txt
+echo "   - 2nd Dev/ Telegram       : @sam_sfx"  | tee -a log-install.txt
+echo "   - Menu Dev/ Telegram      : @kedairare"  | tee -a log-install.txt
+echo ""
+echo "                         Haram Untuk Dijual                                 " | tee -a log-install.txt
+echo "                       Siapa beli, dia kena scam                            " | tee -a log-install.txt
+echo "------------------Script Created By CTechDidik & SamVPN---------------------" | tee -a log-install.txt
+
+sleep 3
+echo '============================================='
+echo '      SISTEM AKAN REBOOT SEBENTAR LAGI'
+echo '============================================='
+echo ""
+rm -f /root/setup.sh
+rm -f /root/.bash_history
+echo " Reboot 15 Sec, Setelah ON type menu"
 sleep 15
-rm -f setup.sh
 reboot
